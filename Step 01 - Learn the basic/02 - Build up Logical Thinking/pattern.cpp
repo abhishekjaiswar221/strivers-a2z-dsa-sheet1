2f3841985bba228983d9d7b0d9f6a9a3ba452780
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2025-04-14 17:08:52 +0530

    Add solution for pattern1-Rectangular Star Pattern

--- Step 01 - Learn the basic/02 - Build up Logical Thinking/pattern.cpp
+++ Step 01 - Learn the basic/02 - Build up Logical Thinking/pattern.cpp
@@ -1,7 +1,43 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+/*
+
+Patterns:
+- To print pattern we always we nested loops.
+- Somehow connect the rows to the columns by forming a logic.
+- Observe symmetry in the pattern or check if a pattern is a combination of
+two or more similar patterns.
+
+
+Pattern 1:
+Input: N = 3
+Output:
+* * *
+* * *
+* * *
+
+*/
+
+void pattern1(int n) {
+  for (int i = 0; i < n; i++) {   // Outer loop for rows
+    for (int j = 0; j < n; j++) { // Inner loop for columns
+      cout << "*";                // Print a single star
+    }
+    cout << "\n"; // Move to the next line after each row
+  }
+}
+
 int main() {
-  cout << "Pattern";
-  return 0;
+  cout << "Enter the number of test cases: ";
+  int test;
+  cin >> test;
+
+  // Loop through each test case
+  for (int i = 0; i < test; i++) {
+    cout << "Enter the number (N): ";
+    int num;
+    cin >> num;
+    pattern1(num); // Call function to print pattern1
+  }
 }
